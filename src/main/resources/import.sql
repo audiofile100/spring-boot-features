c7c917c08b37b992104f2c528bd403ae9c979344
audiofile100 <amplified100@gmail.com>
2023-02-03 12:16:44 -0700

    data seed

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO `books` (publisher, title, year) VALUES ('Okta', 'OAuth 2.0 Simplified', '2021');
+INSERT INTO `books` (publisher, title, year) VALUES ('Manning', 'OAuth 2 in Action', '2017');
+INSERT INTO `books` (publisher, title, year) VALUES ('Wiley', 'OCP Oracle Certified Professional Java SE 17 Developer Study Guide', '2022');
+INSERT INTO `books` (publisher, title, year) VALUES ('Packt', 'Elevating Game Experiences with Unreal Engine 5', '2022');
+INSERT INTO `books` (publisher, title, year) VALUES ('Manning', 'Neo4j in Action', '2015');
