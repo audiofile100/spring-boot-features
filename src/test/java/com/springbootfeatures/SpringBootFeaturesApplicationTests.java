c1d7e5523d42d116f7fac01c3f5c47ba5f810edb
audiofile100 <amplified100@gmail.com>
2023-02-03 10:54:23 -0700

    hello spring boot

---
+++ src/test/java/com/springbootfeatures/SpringBootFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootfeatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFeaturesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
